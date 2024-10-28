e86b81a9b1200ccb9e0736c829cd8e2148a90914
tsukumi <tsukumijima@users.noreply.github.com>
2024-10-29 04:44:23 +0900

    Update: AIVM マニフェストスキーマを更新

--- src/schemas/AivmManifest.ts
+++ src/schemas/AivmManifest.ts
@@ -7,8 +7,8 @@ import Utils from '@/utils';
 
 
 export const ModelArchitectureSchema = z.enum([
-    'Style-Bert-VITS2',
-    'Style-Bert-VITS2 (JP-Extra)',
+    'Style-Bert-VITS2',  // 対応言語: "ja", "en-US", "zh-CN"
+    'Style-Bert-VITS2 (JP-Extra)',  // 対応言語: "ja"
 ]);
 
 export const ModelFormatSchema = z.enum([
@@ -36,24 +36,27 @@ export const AivmManifestSchema = z.object({
     // AIVM マニフェストのバージョン (ex: 1.0)
     // 現在は 1.0 のみサポート
     manifest_version: z.string().regex(/^1\.0$/),
-    // 音声合成モデルの名前
-    name: z.string().min(1),
-    // 音声合成モデルの説明 (省略時は空文字列になる)
-    description: z.string().default(''),
-    // 音声合成モデルの作成者名のリスト (省略時は空リストになる)
-    creators: z.array(z.string()).default([]),
-    // 音声合成モデルの利用規約 (Markdown 形式 / 省略時は空文字列になる)
-    // カスタム利用規約を設定する場合を除き、原則各ライセンスへの URL リンクのみを記述する
-    // 例: [CC BY-SA 4.0](https://creativecommons.org/licenses/by-sa/4.0/)
-    terms_of_use: z.string().default(''),
+    // 音声合成モデルの名前 (最大 80 文字)
+    // 音声合成モデル内の話者が 1 名の場合は話者名と同じ値を設定すべき
+    name: z.string().min(1).max(80),
+    // 音声合成モデルの簡潔な説明 (最大 140 文字 / 省略時は空文字列を設定)
+    description: z.string().max(140).default(''),
+    // 音声合成モデルの作成者名のリスト (省略時は空リストを設定)
+    // 作成者名には npm package.json の "author", "contributors" に指定できるものと同じ書式を利用できる
+    // 例: ["John Doe", "Jane Doe <jane.doe@example.com>", "John Doe <john.doe@example.com> (https://example.com)"]
+    creators: z.array(z.string().min(1).max(255)).default([]),
+    // 音声合成モデルのライセンス情報 (Markdown 形式またはプレーンテキスト / 省略時は null を設定)
+    // AIVM 仕様に対応するソフトでライセンス情報を表示できるよう、Markdown 形式またはプレーンテキストでライセンスの全文を設定する想定
+    // 社内のみでの利用など、この音声合成モデルの公開・配布を行わない場合は null を設定する
+    license: z.string().min(1).nullable().default(null),
     // 音声合成モデルのアーキテクチャ (音声合成技術の種類)
     model_architecture: ModelArchitectureSchema,
     // 音声合成モデルのモデル形式 (Safetensors または ONNX)
     // AIVM ファイル (.aivm) のモデル形式は Safetensors 、AIVMX ファイル (.aivmx) のモデル形式は ONNX である
     model_format: ModelFormatSchema,
-    // 音声合成モデル学習時のエポック数 (省略時は null になる)
+    // 音声合成モデル学習時のエポック数 (省略時は null を設定)
     training_epochs: z.number().int().nonnegative().nullable().default(null),
-    // 音声合成モデル学習時のステップ数 (省略時は null になる)
+    // 音声合成モデル学習時のステップ数 (省略時は null を設定)
     training_steps: z.number().int().nonnegative().nullable().default(null),
     // 音声合成モデルを一意に識別する UUID
     uuid: z.string().uuid(),
@@ -61,34 +64,36 @@ export const AivmManifestSchema = z.object({
     version: z.string().regex(Utils.SEMVER_REGEX),
     // 音声合成モデルの話者情報 (最低 1 人以上の話者が必要)
     speakers: z.array(z.object({
-        // 話者の名前
-        name: z.string().min(1),
+        // 話者の名前 (最大 80 文字)
+        // 音声合成モデル内の話者が 1 名の場合は音声合成モデル名と同じ値を設定すべき
+        name: z.string().min(1).max(80),
         // 話者のアイコン画像 (Data URL)
         // 画像ファイル形式は 512×512 の JPEG (image/jpeg)・PNG (image/png) のいずれか (JPEG を推奨)
         icon: z.string().regex(/^data:image\/(jpeg|png);base64,[A-Za-z0-9+/=]+$/),
-        // 話者の対応言語のリスト (ja, en, zh のような ISO 639-1 言語コード)
-        supported_languages: z.array(z.string().length(2)),
+        // 話者の対応言語のリスト (BCP 47 言語タグ)
+        // 例: 日本語: "ja", アメリカ英語: "en-US", 標準中国語: "zh-CN"
+        supported_languages: z.array(z.string().regex(/^[a-z]{2,3}(?:-[A-Z]{4})?(?:-(?:[A-Z]{2}|\d{3}))?(?:-(?:[A-Za-z0-9]{5,8}|\d[A-Za-z0-9]{3}))*(?:-[A-Za-z](?:-[A-Za-z0-9]{2,8})+)*(?:-x(?:-[A-Za-z0-9]{1,8})+)?$/)),
         // 話者を一意に識別する UUID
         uuid: z.string().uuid(),
         // 話者のローカル ID (この音声合成モデル内で話者を識別するための一意なローカル ID で、uuid とは異なる)
         local_id: z.number().int().nonnegative(),
         // 話者のスタイル情報 (最低 1 つ以上のスタイルが必要)
         styles: z.array(z.object({
-            // スタイルの名前
-            name: z.string().min(1),
+            // スタイルの名前 (最大 20 文字)
+            name: z.string().min(1).max(20),
             // スタイルのアイコン画像 (Data URL, 省略可能)
             // 省略時は話者のアイコン画像がスタイルのアイコン画像として使われる想定
             // 画像ファイル形式は 512×512 の JPEG (image/jpeg)・PNG (image/png) のいずれか (JPEG を推奨)
             icon: z.string().regex(/^data:image\/(jpeg|png);base64,[A-Za-z0-9+/=]+$/).nullable().default(null),
             // スタイルの ID (この話者内でスタイルを識別するための一意なローカル ID で、uuid とは異なる)
             local_id: z.number().int().min(0).max(31),  // 最大 32 スタイルまでサポート
-            // スタイルのボイスサンプル (省略時は空配列になる)
+            // スタイルごとのボイスサンプル (省略時は空リストを設定)
             voice_samples: z.array(z.object({
                 // ボイスサンプルの音声ファイル (Data URL)
                 // 音声ファイル形式は WAV (audio/wav, Codec: PCM 16bit)・M4A (audio/mp4, Codec: AAC-LC) のいずれか (M4A を推奨)
                 audio: z.string().regex(/^data:audio\/(wav|mp4);base64,[A-Za-z0-9+/=]+$/),
                 // ボイスサンプルの書き起こし文
-                // 書き起こし文は音声ファイルの発話内容と一致している必要がある
+                // 書き起こし文は音声ファイルでの発話内容と一致している必要がある
                 transcript: z.string().min(1),
             })).default([]),
         })),
@@ -102,7 +107,7 @@ export const DefaultAivmManifest: AivmManifest = {
     name: 'Model Name',
     description: '',
     creators: [],
-    terms_of_use: '',
+    license: null,
     model_architecture: 'Style-Bert-VITS2 (JP-Extra)',
     model_format: 'Safetensors',
     training_epochs: null,
