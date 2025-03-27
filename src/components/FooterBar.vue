88d16e4d7be6abf9311c0d7ae9fd3cbeab339f0b
tsukumi <tsukumijima@users.noreply.github.com>
2025-03-27 22:45:37 +0900

    Update: プライバシーポリシーの URL を修正

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -42,7 +42,7 @@
                 <div class="footer__bottom-section">
                     <div class="footer__company-links">
                         <a href="https://jpchain.co.jp/news/2024-11-19-aivisproject-terms/" target="_blank">利用規約</a>
-                        <a href="https://jpchain.co.jp/news/2024-11-11-privacy-policy/" target="_blank">プライバシーポリシー</a>
+                        <a href="https://jpchain.co.jp/privacy-policy/" target="_blank">プライバシーポリシー</a>
                         <a href="https://jpchain.co.jp/" target="_blank">会社概要</a>
                     </div>
                     <div class="footer__copyright">© 2024-{{ new Date().getFullYear() }} JPchain Inc.</div>
