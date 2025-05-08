5b9a3cc3758f03adac1c7091be07325b9ee2d403
tsukumi <tsukumijima@users.noreply.github.com>
2025-05-08 15:42:04 +0900

    Update: 会社概要とコピーライト表記を Walkers に移行
    プライバシーポリシーとかはまだ移行先ページがないので当面残置せざるを得ない

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -43,9 +43,9 @@
                     <div class="footer__company-links">
                         <a href="https://jpchain.co.jp/news/2024-11-19-aivisproject-terms/" target="_blank">利用規約</a>
                         <a href="https://jpchain.co.jp/privacy-policy/" target="_blank">プライバシーポリシー</a>
-                        <a href="https://jpchain.co.jp/" target="_blank">会社概要</a>
+                        <a href="https://walker-s.co.jp/" target="_blank">会社概要</a>
                     </div>
-                    <div class="footer__copyright">© 2024-{{ new Date().getFullYear() }} JPchain Inc.</div>
+                    <div class="footer__copyright">© 2024-{{ new Date().getFullYear() }} Walkers Inc.</div>
                 </div>
             </div>
         </div>
