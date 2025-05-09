448e706c73ce26cbda20c7ce511ad8243e433dc1
tsukumi <tsukumijima@users.noreply.github.com>
2025-05-10 01:18:55 +0900

    Fix: リリース時期の記述を修正

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -27,7 +27,7 @@
                         モデルサイズを簡単に削減できますので、以前音声合成モデルを制作された方もぜひ一度お試しください！🙏
                     </div>
                     <div>
-                        ⚠️ Style-Bert-VITS2 で作成した Safetensors モデルを ONNX に変換する機能はもうしばらくお待ちください！4月中に実装予定です。<br>
+                        ⚠️ Style-Bert-VITS2 で作成した Safetensors モデルを ONNX に変換する機能はもうしばらくお待ちください！5月中に実装予定です。<br>
                     </div>
                 </div>
             </div>
@@ -60,7 +60,7 @@
             <strong>Style-Bert-VITS2 で作成した Safetensors モデルはあるが、ONNX モデルをお持ちでない場合、下のフォームからかんたんに変換できます。</strong><br>
             「変換開始」ボタンを押すと、モデルファイルが変換サーバーに送信され、変換処理が始まります。変換には数分程度かかります。<br>
             変換が完了すると、ONNX モデルが自動的にダウンロードされます。変換後にサーバー上のファイルは直ちに削除されますので、ご安心ください。<br>
-            ※現在開発中につきまだ動作しません（2025年4月中に実装予定）。今すぐ Style-Bert-VITS2 で作成したモデルを ONNX に変換したい方は最新の dev ブランチに切り替えた上で <a class="link" href="https://github.com/litagin02/Style-Bert-VITS2/blob/dev/convert_onnx.py" target="_blank">こちらの ONNX 変換スクリプト</a> をご利用ください（デベロッパー向け）。<br>
+            ※現在開発中につきまだ動作しません（2025年5月中に実装予定）。今すぐ Style-Bert-VITS2 で作成したモデルを ONNX に変換したい方は最新の dev ブランチに切り替えた上で <a class="link" href="https://github.com/litagin02/Style-Bert-VITS2/blob/dev/convert_onnx.py" target="_blank">こちらの ONNX 変換スクリプト</a> をご利用ください（デベロッパー向け）。<br>
             <div class="mt-3 d-flex align-center" style="gap: 12px;">
                 <div class="d-flex flex-column" style="flex: 1;">
                     <v-file-input variant="solo-filled" density="compact" show-size hide-details
